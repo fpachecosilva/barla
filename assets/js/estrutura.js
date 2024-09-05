5e2a2120eb6162854fbcf10eb2fc72b6aad8f9f4
Fabio <fabio_pachecosilva@hotmail.com>
2024-09-05 04:45:55 -0300

    ajustes finais

--- assets/js/estrutura.js
+++ assets/js/estrutura.js
@@ -1,5 +1,5 @@
 var swiperOne = new Swiper('.swiperOne', {    
-  loop: false,
+  loop: true,
 	effect: "fade",
 
   pagination: {
@@ -13,7 +13,7 @@ var swiperOne = new Swiper('.swiperOne', {
 });
 
 var swiperSecao4 = new Swiper('.swiperSecao4', {    
-  loop: false,
+  loop: true,
 	effect: "fade",
   autoHeight: true,
 
@@ -68,3 +68,66 @@ navLinks.forEach(link => {
     header.classList.remove('open');
   });
 });
+
+
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function() {
+    document.querySelectorAll('a').forEach(item => item.classList.remove('active'));
+    this.classList.add('active');
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  
+
+    const sections = document.querySelectorAll('.secao');
+    const navLinks = document.querySelectorAll('nav a');
+  
+    window.addEventListener('scroll', () => {
+        let current = '';
+  
+        sections.forEach(section => {          
+            if (section.getAttribute('id') === 'leiterosas') return;
+  
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            
+            if (window.scrollY >= sectionTop - sectionHeight / 3) {
+              current = section.getAttribute('id');
+            }          
+        });
+        
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+            if (link.getAttribute('href') === `#${current}`) {
+                link.classList.add('active');
+            }
+        });  
+    });
+
+  
+
+});
+
+window.addEventListener('scroll', function() {
+  var logo = document.querySelector('.logo');
+  var scrollPosition = window.scrollY;
+  var screenWidth = window.innerWidth;
+
+  if (screenWidth < 769) {
+      if (scrollPosition > 50) {
+          logo.classList.add('intop');
+      } else {
+          logo.classList.remove('intop');
+      }
+  } else {
+      logo.classList.remove('intop');
+  }
+});
+
+
+
+
+
+
